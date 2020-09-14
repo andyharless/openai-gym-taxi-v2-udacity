5b403908bdcbd5414ecd2796e0b5d923aab2e82b
Andy Harless <aharless@kr.af.mil>
2020-09-14 08:52:33 -0400

    faster but still not higher

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ from math import exp
 import random
 
 # Control parameters
-n_episodes = 100000
+n_episodes = 80000
 nruns = 1
 medsub = nruns // 2
 
 # Learning parameters
+beta=.70
+c1=.02
+c2=8
 alpha=.7
 gamma=.5
 a = -.005
-b = +.005
-eps_min = 2e-5
-epfunc = lambda i: max(eps_min, exp(a - b*i))
+b = .00022
+c = 0
+eps_min = 0
+epfunc = lambda i: max(eps_min, exp(a - b*i + c*i**2))
 
 # Cheating by using a successful seed
 # (Median result for v2 is 9.44 with multiple random runs.)
@@ -28,6 +32,10 @@ local_seed = 0
 start = 8
 
 print("\n\nBegin Taxi-v2 learning...")
+print("Parameters:")
+print("path_memory_decay={}, Q_weight={}, recency_weight={}, eps_start={}, eps_decay={}".format(
+        beta, c1, c2, a, b))
+print("learning_rate={}, discount_rate={}, min_stochasticity={}".format(alpha, gamma, eps_min))
 
 # Multiple sample runs (but in this case only the best one)
 for i in range(start, start+nruns):
@@ -41,13 +49,13 @@ for i in range(start, start+nruns):
     env.seed(10000*i+local_seed)
 
     # Run the learning problem
-    agent = Agent(alpha=alpha, gamma=gamma, get_epsilon=epfunc)
-    avg_rewards, best_avg_reward = interact(env, agent, n_episodes, show_progress=10000)
+    agent = Agent(alpha=alpha, gamma=gamma, get_epsilon=epfunc, c1=c1, c2=c2, beta=beta)
+    avg_rewards, best_avg_reward = interact(env, agent, n_episodes, show_progress=10000, endline='\n')
     best_avg_rewards.append(best_avg_reward)
 
     # Monitor results after each run
     print("\rRun {}/{}, average so far={}".format(i, nruns,
-                                        sum(best_avg_rewards)/len(best_avg_rewards)), end="")
+                                        sum(best_avg_rewards)/len(best_avg_rewards)), end="\n")
 
 print('\nLocal seed: ', local_seed)
 print('Average: ', sum(best_avg_rewards)/len(best_avg_rewards))
